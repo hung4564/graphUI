6ee5c057fdfee1f6436bb460f92d2122ae7f30c4
hung4564 <hphamviet83@gmail.com>
2018-03-01 17:03:34 +0700

    them ham kiem tra tinh lien thong va day du cua do thi tren danh sach ke

--- graph_toanroirac/Program.cs
+++ graph_toanroirac/Program.cs
@@ -59,12 +59,14 @@ namespace graph_toanroirac
                 {
                     // Doc file, lay tung hang roi tach tung cot cho vao ma tran
                     string matrix = sr.ReadToEnd();
+                    while(matrix.IndexOf("\r")>0)
+                    matrix=matrix.Remove(matrix.IndexOf("\r"), 1);
                     string[] rows = matrix.Split('\n');
                     n = rows.Count();
                     for (int i = 0; i < rows.Count(); i++)
                     {
                         string[] columns = rows[i].Split(' ');
-                        for (int j = 0; j < columns.Count()-1; j++)//ki tu cuoi la /r??(ko hieu vi sao lai co)
+                        for (int j = 0; j < columns.Count(); j++)
                         {
                             a[i, j] = int.Parse(columns[j]);
                         }
@@ -103,12 +105,62 @@ namespace graph_toanroirac
                 }
             }
         }
+        /// <summary>
+        /// Kiểm tra tính liên thông của ma trận
+        /// </summary>
+        /// <param name="A">Ma trân vuông cần kiểm tra</param>
+        /// <param name="n">Độ dài cạnh</param>
+        /// <returns>Trả về đúng nếu liên thông</returns>
+        static bool Is_lienthong(int[,] A, int n)
+        {
+            int[] DanhDau = new int[n];
+            int ThanhCong;
+            int Dem = 0;
+            DanhDau[0] = 1;         //Đánh dấu đỉnh đầu
+            Dem++;              //Đểm số đỉnh đã đánh dấu là 1
+            do
+            {
+                ThanhCong = 1;      //Giả sử không còn khả năng loang
+                for (int i = 0; i < n; i++)
+                    if (DanhDau[i] == 1)
+                    {
+                        for (int j = 0; j < n; j++)
+                            if (DanhDau[j] == 0 && A[i,j] > 0)
+                            {
+                                DanhDau[j] = 1;
+                                ThanhCong = 0;  //Thực tế còn khả năng loang
+                                Dem++;
+                                if (Dem == n) return true;
+                            }
+                    }
+            } while (ThanhCong == 0);	//Lặp lại cho đến khi không còn khả năng loang
+            return false;
+        }
+        /// <summary>
+        /// Kiểm tra tính đầy đủ của ma trận
+        /// </summary>
+        /// <param name="a">ma trận cần kiểm tra</param>
+        /// <param name="n">số cạnh của ma trận</param>
+        /// <returns>Trả về đúng nếu là ma trận đầy đủ</returns>
+        static bool Is_daydu(int[,] a,int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (a[i, j] == 0) return false;
+                }
+            }
+            return true;
+        }
+
         static void Main(string[] args)
         {
             int n=0;
             int[,] matrix = new int[50, 50];
             ReadMatrix(matrix,out n);
             XuatMatrix(matrix, n);
+            if (Is_lienthong(matrix,n)) Console.WriteLine("Lien thong"); else Console.WriteLine("Khong lien thong");
             Console.ReadKey();
         }
     }
